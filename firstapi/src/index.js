a7bd2581398bee898e9d1b4d9b4549327e6fa668
LeoSanGo <souoleo@gmail.com>
2025-03-18 16:47:03 -0300

    Adicionando query params URLSearchParams

--- firstapi/src/index.js
+++ firstapi/src/index.js
@@ -1,20 +1,27 @@
 const http = require("http");
-
 const routes = require("./routes");
+const { URL } = require("url");
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint ${request.url}`);
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+  console.log(
+    `Request method: ${request.method} | Endpoint ${parsedUrl.pathname}`
+  );
 
   const route = routes.find(
     (routeObj) =>
-      routeObj.endpoint === request.url && routeObj.method === request.method
+      routeObj.endpoint === parsedUrl.pathname &&
+      routeObj.method === request.method
   );
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+    console.log(request.query);
+
     route.handler(request, response);
   } else {
     response.writeHead(404, { "content-type": "text/html" });
-    response.end(`Cannot ${request.method} ${request.url} `);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname} `);
   }
 });
 
