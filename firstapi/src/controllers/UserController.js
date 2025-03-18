640662199c49dd19e7d00cdb67120f09cd9b9503
LeoSanGo <souoleo@gmail.com>
2025-03-18 17:37:46 -0300

    handler getUsersById

--- firstapi/src/controllers/UserController.js
+++ firstapi/src/controllers/UserController.js
@@ -13,4 +13,16 @@ module.exports = {
     response.writeHead(200, { "content-type": "application/json" });
     response.end(JSON.stringify(sortedUsers));
   },
+  getUsersById(request, response) {
+    const { id } = request.params;
+
+    const user = users.find((user) => user.id === Number(id));
+    if (!user) {
+      response.writeHead(400, { "content-type": "application/json" });
+      response.end(JSON.stringify({ error: "User not found" }));
+    } else {
+      response.writeHead(200, { "content-type": "application/json" });
+      response.end(JSON.stringify(user));
+    }
+  },
 };
